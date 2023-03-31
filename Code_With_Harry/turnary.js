7dabdde914bf2cf0dc19b40773f1fcc598d92027
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-03-31 22:47:53 +0500

    String Methods

--- Code_With_Harry/turnary.js
+++ Code_With_Harry/turnary.js
@@ -1,16 +1,27 @@
 // console.log((1>2) ? '1 is greater than 2':'1 is NOT greater than 2');
 
-let obj = {
-    name: "ARK",
-    age: 19,
-    isMale: true
-}
-for(let keys in obj){
-    console.log(`${keys}: ${obj[keys]}`);
-}
+// let obj = {
+//     name: "ARK",
+//     age: 19,
+//     isMale: true
+// }
+// for(let keys in obj){
+//     console.log(`${keys}: ${obj[keys]}`);
+// }
 
 
-let arr = ['lemon', 'banana', 'apple', 'strawberry']
-for(let item of arr){
-    console.log(`${item}`)
+// let arr = ['lemon', 'banana', 'apple', 'strawberry']
+// for(let item of arr){
+//     console.log(`${item}`)
+// }
+
+let name = 'I am ARK'
+console.log(name.concat("Miss Thing Slay.").toLowerCase().replace('a', 'Slay').toUpperCase())
+
+for(let i=0; i<name.length; i++){
+    console.log(name[i]);
 }
+
+let ex = "Example"
+console.log(`This is an ${ex.toUpperCase()}`)
+
