a4f988171b9f8d98432e293f5a1aeb6bc8cb6916
Abdul Rehman Khan <khan456.tdm@gmail.com>
2023-04-01 23:43:44 +0500

    Task 1 Solved

---
+++ Angela/Tasks/1_Task.js
@@ -0,0 +1,11 @@
+/* 
+* Task was to take name from user. 
+* capitalize the first letter of their name.
+* display the rest of the name as is.
+*/
+let name = prompt("Enter name: ");
+let firstLetter = (name.slice(0,1)).toUpperCase();
+let restName = name.slice(1, name.length);
+
+name = firstLetter.concat(restName);
+alert(`Hello, ${name}`);
