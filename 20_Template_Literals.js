661cfcd8ab6c0b412ef24e14c989b615912995c1
Abdul Rehman Khan <khan456.tdm@gmail.com>
2024-03-25 10:46:07 +0500

    ✨

--- 20_Template_Literals.js
+++ 20_Template_Literals.js
@@ -1,5 +1,6 @@
 /*
-    Template Literals: Used to perform better and simpler concatenation operator
+    Template Literals: Used to perform better and 
+    simpler concatenation operator
 */
 let greetings = "Hello!", phrase = "You get:"
 console.log(`${greetings} ${phrase} ${1+1+1} Stars!`)
